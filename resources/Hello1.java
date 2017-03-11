5fe0ef26b69001b79ef376f6088dca9638f07471
gurnoors <gurnoor@gmail.com>
2017-03-10 22:57:14 -0800

    Handled additional cases, constructs.

--- resources/Hello1.java
+++ resources/Hello1.java
@@ -3,6 +3,11 @@ package com.gurnoors.cmpe202.uml;
 class Hello1 {
     private String str ;
     MyClass foo;
+    
+    public Hello1() {
+		// TODO Auto-generated constructor stub
+	}
+    
     public String getStr() { return this.str ; }
     public void   setStr(String value) { this.str = value ; }
 }
--- src/main/java/com/gurnoors/cmpe202/uml/practice/MyVisitor.java
+++ src/main/java/com/gurnoors/cmpe202/uml/practice/MyVisitor.java
@@ -2,17 +2,17 @@ package com.gurnoors.cmpe202.uml.practice;
 
 import java.util.List;
 
+import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MemberValuePair;
 import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-
-import javaslang.control.Match.MatchMonad.Of;
 
 public class MyVisitor extends VoidVisitorAdapter<Void> {
 	// @Override
@@ -30,6 +30,12 @@ public class MyVisitor extends VoidVisitorAdapter<Void> {
 		System.out.println(n.getAnonymousClassBody());
 		super.visit(n, arg);
 	}
+	
+	@Override
+	public void visit(ImportDeclaration n, Void arg) {
+		super.visit(n, arg);
+		
+	}
 
 	@Override
 	public void visit(ClassOrInterfaceDeclaration n, Void arg) {
@@ -41,23 +47,16 @@ public class MyVisitor extends VoidVisitorAdapter<Void> {
 				System.out.println("Its a Field!!!");
 			}else if (member instanceof MethodDeclaration) {
 				System.out.println("A Method!!!");
+			}else if (member instanceof ConstructorDeclaration){
+				System.out.println("A Constructor!!!");
 			}else{
 				System.out.println("Oh I forgot to consider: "+member.getClass().getName());
 			}
 			System.out.println(member.getClass().getName());
+			System.out.println(member.toString());
 			System.out.println();
 			
 		}
-		
-//		List<Node> children  = n.getChildNodes();
-//		for(Node child : children){
-//			System.out.println("Node Type: "+child.getClass().getName());
-//			System.out.println(child.toString());
-
-			
-			
-//			System.out.println();
-//		}
 		super.visit(n, arg);
 	}
 }
