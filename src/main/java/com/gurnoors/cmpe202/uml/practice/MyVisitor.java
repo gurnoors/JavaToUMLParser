9a924d38ac8456cf9fb972b57be88475a76be410
gurnoors <gurnoor@gmail.com>
2017-03-11 23:44:07 -0800

    Refactor

--- src/main/java/com/gurnoors/cmpe202/uml/practice/MyVisitor.java
+++ src/main/java/com/gurnoors/cmpe202/uml/practice/MyVisitor.java
@@ -22,7 +22,6 @@ public class MyVisitor extends VoidVisitorAdapter<Void> {
 	// this method will be called for all methods in this
 	// CompilationUnit, including inner class methods */
 	// System.out.println(n.getName());
-	// super.visit(n, arg);
 
 	@Override
 	public void visit(ObjectCreationExpr n, Void arg) {
