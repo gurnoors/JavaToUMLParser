4b3ae3fb955e87beae492fbf3b545c821218fd3a
gurnoors <gurnoor@gmail.com>
2017-04-01 23:41:43 -0700

    Helo Reflection

---
+++ src/main/java/com/gurnoors/cmpe202/uml/practice/reflection/HelloReflect.java
@@ -0,0 +1,17 @@
+package com.gurnoors.cmpe202.uml.practice.reflection;
+
+import java.lang.reflect.Method;
+
+import com.gurnoors.cmpe202.uml.practice.HelloJavaParser;
+
+public class HelloReflect {
+
+	public static void main(String[] args) {
+		Method[] methods = HelloJavaParser.class.getMethods();
+
+		for(Method method : methods){
+		    System.out.println("method = " + method.getName());
+		}
+	}
+
+}
