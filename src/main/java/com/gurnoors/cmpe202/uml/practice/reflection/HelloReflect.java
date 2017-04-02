2109cfc5c71fd8800e9660fd7cda5c1b7663861c
gurnoors <gurnoor@gmail.com>
2017-04-01 23:48:29 -0700

    refacotr

--- src/main/java/com/gurnoors/cmpe202/uml/practice/reflection/HelloReflect.java
+++ src/main/java/com/gurnoors/cmpe202/uml/practice/reflection/HelloReflect.java
@@ -11,13 +11,14 @@ public class HelloReflect {
 		Method[] methods = HelloJavaParser.class.getMethods();
 
 		for(Method method : methods){
+			System.out.println(method.getModifiers());
 		    System.out.println("---------method = " + method.getName());
 		    System.out.println(method.getParameterCount());
 		    for(Parameter parameter : method.getParameters()){
 		    	System.out.println("Param-->");
 		    	System.out.println(parameter.toString());
 		    }
-		    System.out.println(method.getModifiers());
+		    
 		  
 		    System.out.println("--------"+method.getName() + " ends\n");
 		}
