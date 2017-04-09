0b90dd0aebc54a6b45c79ade3fa155ff63583618
gurnoors <gurnoor@gmail.com>
2017-04-08 19:33:11 -0700

    refactor

--- src/main/java/com/gurnoors/cmpe202/uml/practice/reflection/HelloReflect.java
+++ src/main/java/com/gurnoors/cmpe202/uml/practice/reflection/HelloReflect.java
@@ -13,16 +13,16 @@ public class HelloReflect {
 
 		for(Method method : methods){
 			System.out.println(method.getModifiers());
-		    System.out.println("---------method = " + method.getName());
-		    System.out.println(method.getParameterCount());
+		    
 		    for(Parameter parameter : method.getParameters()){
 		    	System.out.println("Param-->");
 		    	System.out.println(parameter.toString());
 		    }
 		    
-		  
+		    
 		    System.out.println("--------"+method.getName() + " ends\n");
 		}
 	}
+	
 
 }
