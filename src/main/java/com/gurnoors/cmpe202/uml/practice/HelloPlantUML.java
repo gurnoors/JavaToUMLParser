3ea8300433c34088a424fa8fa59dcf13124663fe
gurnoors <gurnoor@gmail.com>
2017-04-22 23:31:00 -0700

    Add test case 5

--- src/main/java/com/gurnoors/cmpe202/uml/practice/HelloPlantUML.java
+++ src/main/java/com/gurnoors/cmpe202/uml/practice/HelloPlantUML.java
@@ -2,15 +2,18 @@ package com.gurnoors.cmpe202.uml.practice;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class HelloPlantUML {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		hi();
 	}
 
-	private static void hi() throws FileNotFoundException {
+	private static void hi() throws IOException {
 		FileReader reader = new FileReader("foo.java");
+		reader.read();
+		reader.close();
 	}
 
 }
