21a83cb78ce0327baf57a7f17659855c6cfeeb1e
gurnoors <gurnoor@gmail.com>
2017-04-22 23:30:16 -0700

    PLANTUML edge cases

--- src/main/java/com/gurnoors/cmpe202/uml/practice/HelloPlantUML.java
+++ src/main/java/com/gurnoors/cmpe202/uml/practice/HelloPlantUML.java
@@ -1,14 +1,15 @@
 package com.gurnoors.cmpe202.uml.practice;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class HelloPlantUML {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		hi();
 	}
 
-	private static void hi() {
+	private static void hi() throws FileNotFoundException {
 		FileReader reader = new FileReader("foo.java");
 	}
 
